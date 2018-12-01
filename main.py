20f5378a886a2589732518564e2b0fc02d4cfb6c
Dmitry315 <vgurskoy@mail.ru>
2018-12-01 20:00:07 +0300

    add engineer calculator backend

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sys import argv, exit
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
-from math import sqrt, factorial, pow
+from math import *
 
 widgets = []
 
@@ -71,7 +71,8 @@ class CommonCalculator(QDialog):
     # add symbol in line
     def sym(self):
         x = self.line.text()
-        self.line.setText(x + self.sender().text())
+        y = self.sender().text()
+        self.line.setText(x + y)
 
     # Main operations: addition, subtraction, division and multiplication
     def add(self):
@@ -80,9 +81,12 @@ class CommonCalculator(QDialog):
         self.operation = '+'
 
     def sub(self):
-        self.first_num = self.line.text()
-        self.line.setText('')
-        self.operation = '-'
+        if self.line.text() == '':
+            self.line.setText('-')
+        elif self.line.text() != '-':
+            self.first_num = self.line.text()
+            self.line.setText('')
+            self.operation = '-'
 
     def div(self):
         self.first_num = self.line.text()
@@ -115,6 +119,9 @@ class CommonCalculator(QDialog):
                 self.line.setText(str(x / y))
             elif self.operation == '^':
                 self.line.setText(str(pow(x, y)))
+            # Engineer calculator option
+            elif self.operation == 'mod':
+                self.line.setText(str(x % y))
         except Exception as err:
             self.line.setText('ERROR')
 
@@ -153,6 +160,115 @@ class EngineerCalculator(CommonCalculator):
         EngineerCalculator.widgets.append(self)
         widgets.append(self)
         super().__init__('Engineer.ui')
+        # trigonometric functions
+        # There is checkbox, if it is on
+        # result will be calculated in degrees and
+        # in radians in another option.
+        self.btn_sin.clicked.connect(self.sin)
+        self.btn_cos.clicked.connect(self.cos)
+        self.btn_tg.clicked.connect(self.tg)
+        self.btn_arcsin.clicked.connect(self.arcsin)
+        self.btn_arccos.clicked.connect(self.arccos)
+        self.btn_arctg.clicked.connect(self.arctg)
+        # logarithm button
+        self.btn_log.clicked.connect(self.log)
+        # modation button
+        self.btn_mod.clicked.connect(self.mod)
+        # absolute value button
+        self.btn_abs.clicked.connect(self.abs)
+        # math constants
+        self.btn_e.clicked.connect(self.add_constant_e)
+        self.btn_pi.clicked.connect(self.add_constant_pi)
+
+    # common trigonometric functions
+    def sin(self):
+        try:
+            x = float(self.line.text())
+            if self.degrees.isChecked():
+                x = x * pi / 180
+            self.line.setText(str(sin(x)))
+        except Exception as err:
+            self.line.setText('ERROR')
+
+    def cos(self):
+        try:
+            x = float(self.line.text())
+            if self.degrees.isChecked():
+                x = x * pi / 180
+            self.line.setText(str(cos(x)))
+        except Exception as err:
+            self.line.setText('ERROR')
+
+    def tg(self):
+        try:
+            x = float(self.line.text())
+            if self.degrees.isChecked():
+                x = x * pi / 180
+            self.line.setText(str(tan(x)))
+        except Exception as err:
+            self.line.setText('ERROR')
+
+    # inverse trigonometric functions
+    def arcsin(self):
+        try:
+            x = float(self.line.text())
+            x = asin(x)
+            if self.degrees.isChecked():
+                x = x / pi * 180
+            self.line.setText(str(x))
+        except Exception as err:
+            self.line.setText('ERROR')
+
+    def arccos(self):
+        try:
+            x = float(self.line.text())
+            x = acos(x)
+            if self.degrees.isChecked():
+                x = x / pi * 180
+            self.line.setText(str(x))
+        except Exception as err:
+            self.line.setText('ERROR')
+
+    def arctg(self):
+        try:
+            x = float(self.line.text())
+            x = atan(x)
+            if self.degrees.isChecked():
+                x = x / pi * 180
+            self.line.setText(str(x))
+        except Exception as err:
+            self.line.setText('ERROR')
+
+    # logarithm function
+    def log(self):
+        try:
+            x = float(self.line.text())
+            self.line.setText(str(log(x)))
+        except Exception as err:
+            self.line.setText('ERROR')
+
+    # modation operation function
+    def mod(self):
+        self.first_num = self.line.text()
+        self.line.setText('')
+        self.operation = 'mod'
+
+    # absolute value function
+    def abs(self):
+        try:
+            x = float(self.line.text())
+            self.line.setText(str(abs(x)))
+        except Exception as err:
+            self.line.setText('ERROR')
+
+    # constants functions
+    # pi
+    def add_constant_pi(self):
+        self.line.setText(str(pi))
+
+    # e
+    def add_constant_e(self):
+        self.line.setText(str(e))
 
 
 # main
